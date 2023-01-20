61ceedf1cedae5e406540a66cb8492ecf80893c4
mariamdopidze <119893246+mariamdopidze@users.noreply.github.com>
2023-01-19 22:37:57 -0800

    added script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+let personsArray = [
+    {
+    name:"mariam",
+    age:30,
+    address: "saburtalo",
+}
+
+]
+
+console.log(personsArray[0].name);
+console.log(personsArray[0].age);
+console.log(personsArray[0].address);
+
+
+let age = 30
+if(age < 20) {console.log('I am a teenager');}
+else if(age > 20) {console.log('I am an adult');}
+else {console.log('I am an adult');}
+
+
+
+let namberArray =[
+    {age: 32,
+     id:13,
+     namber:25,
+     text:42,
+     firstname:58,   
+      
+    }
+    if (age > 19 || id === 13 ){console.log('more than 19');}
+]
+
+ 
